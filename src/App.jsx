62b596d1ab39723c6f7810719051ce700c834a55
huzaifa434 <saadhuzaifa2497@gmail.com>
2023-10-23 15:43:12 +0600

    initial 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
 import { Outlet } from 'react-router-dom'
 import { createContext} from 'react'; 
+import { useState } from 'react';
 export const myContext = createContext(null)
 function App() {
-  const [user,setUser]
-
-
+  const [user,setUser]=useState(null)
   const context = {
-
+    user,
   }
   return (
     <>
